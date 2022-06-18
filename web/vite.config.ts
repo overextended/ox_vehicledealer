e49e071d41c3ff79aee98a7c3bbb94b5c383a8fd
Luke <sabolukas03@gmail.com>
2022-06-18 12:26:33 +0200

    chore(web): override annoying ecmascript module warnings

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: "./",
-	build: {
-		outDir: "build",
-	},
+  plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "build",
+  },
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
 });
