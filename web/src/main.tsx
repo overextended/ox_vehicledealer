b597fb2a38ba9cac72ee30b652abc1f46845091a
Luke <sabolukas03@gmail.com>
2022-06-19 10:38:00 +0200

    chore(web): add reference background

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,6 +5,17 @@ import { MantineProvider } from "@mantine/core";
 import { VisibilityProvider } from "./providers/VisibilityProvider";
 import App from "./App";
 import "./index.css";
+import { isEnvBrowser } from "./utils/misc";
+
+if (isEnvBrowser()) {
+  const root = document.getElementById("root");
+
+  // https://i.imgur.com/iPTAdYV.png - Night time img
+  root!.style.backgroundImage = 'url("https://i.imgur.com/3pzRj9n.png")';
+  root!.style.backgroundSize = "cover";
+  root!.style.backgroundRepeat = "no-repeat";
+  root!.style.backgroundPosition = "center";
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
