410191059f07be3345696a79e3b236eda20ee4b7
Luke <sabolukas03@gmail.com>
2022-06-19 10:32:22 +0200

    refactor(web): use input for price filter instead of slider

--- web/src/layouts/nav/components/Filters.tsx
+++ web/src/layouts/nav/components/Filters.tsx
@@ -1,12 +1,13 @@
-import { Box, Stack, Slider, Text } from "@mantine/core";
+import { Box, Stack, NumberInput } from "@mantine/core";
 import FilterSlider from "../../../components/FilterSlider";
+import { TbReceipt2 } from "react-icons/tb";
 
 const Filters: React.FC = () => {
   return (
     <>
       <Box mt={15} sx={{ fontWeight: 400 }}>
         <Stack>
-          <FilterSlider label="Price" max={7900000} />
+          <NumberInput label="Max price" hideControls icon={<TbReceipt2 fontSize={20} />} />
           <FilterSlider label="Seats" min={1} max={16} />
           <FilterSlider label="Doors" max={8} min={1} />
         </Stack>
