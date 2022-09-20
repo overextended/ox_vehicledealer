75747b58d560c61b8afea035857c5cf6eaa2d71d
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-09-20 22:40:01 +1000

    feat(data): import export property type

---
+++ data/import export.lua
@@ -0,0 +1,17 @@
+return {
+	['Terminal'] = {
+		sprite = 794,
+		blip = vec(855.0, -3217.0),
+		stashes = {},
+		zones = {
+			{
+				name = 'Import Export',
+				type = 'import/export',
+				vehicles = { automobile = true, bicycle = true, bike = true, quadbike = true },
+				box = true,
+				coords = vec3(855.0, -3217.0, 6.0),
+				size = vec3(18.0, 13.5, 4.0),
+			},
+		},
+	},
+}
