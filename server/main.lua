0ed3eef7529cee4a9b88f69af4e49d2e218293f4
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-09-23 15:38:25 +1000

    feat(server): check against restrictions when buying wholesale

--- server/main.lua
+++ server/main.lua
@@ -40,6 +40,12 @@ RegisterServerEvent('ox_vehicledealer:buyWholesale', function(data)
 
     if not exports.ox_property:isPermitted(player, zone) then return end
 
+    local modelData =  Ox.GetVehicleData(data.model)
+    if not zone.restrictions.type[modelData.type] or not zone.restrictions.class[modelData.class] or (zone.restrictions.weapons ~= nil and zone.restrictions.weapons ~= modelData.weapons) then
+        TriggerClientEvent('ox_lib:notify', player.source, {title = 'Vehicle not available', type = 'error'})
+        return
+    end
+
     -- TODO financial integration
     if true then
         local vehicle = Ox.CreateVehicle({
